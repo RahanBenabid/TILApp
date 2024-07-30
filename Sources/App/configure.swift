4792af174090ede01b2a36929010b0d5a708b39e
RahanBenabid <rahannadime@gmail.com>
2024-07-30 14:33:11 +0100

    changed from postgre to sqlite

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,32 +1,13 @@
-import NIOSSL
 import Fluent
-import FluentPostgresDriver
+// 1
+import FluentSQLiteDriver
 import Vapor
-
 // configures your application
-public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-    
-    // add it to the list of migrations to run
+public func configure(_ app: Application) throws {
+    app.databases.use(.sqlite(.memory), as: .sqlite)
     app.migrations.add(CreateAcronym())
-    
-    // just for providing more informations
     app.logger.logLevel = .debug
-    
-    // runs the migration and waits for the result
-    try await app.autoMigrate().get()
-    
-
+    try app.autoMigrate().wait()
     // register routes
     try routes(app)
 }
