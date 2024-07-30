55258b4e87a7665217f8300ce2a71223e38c0031
RahanBenabid <rahannadime@gmail.com>
2024-07-30 17:00:43 +0100

    added CRUD operations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,13 +1,27 @@
 import Fluent
 // 1
-import FluentSQLiteDriver
+import FluentPostgresDriver
 import Vapor
+
 // configures your application
 public func configure(_ app: Application) throws {
-    app.databases.use(.sqlite(.memory), as: .sqlite)
-    app.migrations.add(CreateAcronym())
-    app.logger.logLevel = .debug
-    try app.autoMigrate().wait()
-    // register routes
-    try routes(app)
+  // 2
+  app.databases.use(.postgres(
+    hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+    username: Environment.get("DATABASE_USERNAME")
+      ?? "vapor_username",
+    password: Environment.get("DATABASE_PASSWORD")
+      ?? "vapor_password",
+    database: Environment.get("DATABASE_NAME")
+      ?? "vapor_database"
+  ), as: .psql)
+  
+  app.migrations.add(CreateAcronym())
+  
+  app.logger.logLevel = .debug
+  
+  try app.autoMigrate().wait()
+  
+  // register routes
+  try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -20,4 +20,65 @@ func routes(_ app: Application) throws {
             acronym
         }
     }
+	
+	// get all the data
+	app.get("api", "acronyms") { req -> EventLoopFuture<[Acronym]> in
+		Acronym.query(on: req.db).all()
+    }
+	
+	// gets the value in the request path
+	app.get("api", "acronyms", ":acronymID") {
+		req -> EventLoopFuture<Acronym> in
+		// find() takes a UUID as a param
+		Acronym.find(req.parameters.get("acronymID"), on: req.db)
+			// since find() returns EventLoopFuture<Acronym?>, we handle this difference by adding unwrap()
+			.unwrap(or: Abort(.notFound))
+	}
+	
+	// update using the UUID
+	app.put("api", "acronyms", ":acronymID") {
+		req -> EventLoopFuture<Acronym> in
+		// decode the request body
+		let updatedAcronym = try req.content.decode(Acronym.self)
+		// find the acronym with the specified UUID
+		return Acronym.find(
+			req.parameters.get("acronymID"),
+			on: req.db)
+		// returns an error if not found
+		.unwrap(or: Abort(.notFound))
+		// waits for the future to complete, flatMap  is used because the operation inside involves another asynchronous operation, saving the updated acronym to the database.
+		.flatMap { acronym in
+			acronym.short = updatedAcronym.short
+			acronym.long = updatedAcronym.long
+			return acronym.save(on: req.db).map {
+				acronym
+			}
+		}
+	}
+	
+	// delete operation that returns an HTTPStatus
+	app.delete("api", "acronyms", ":acronymID") {
+		req -> EventLoopFuture<HTTPStatus> in
+		// exract the ID from the request
+		Acronym.find(req.parameters.get("acronymID"), on: req.db)
+			.unwrap(or: Abort(.notFound))
+		
+			// wait for the acronym to return from the database
+			.flatMap { acronym in
+				// deletes it
+				acronym.delete(on: req.db)
+					// trandforms the result into a 204 code: no result
+					.transform(to: .noContent)
+			}
+	}
+	
+	app.get("api", "acronyms", "search") {
+		req -> EventLoopFuture<[Acronym]> in
+		guard let searchTerm = req.query[String.self, at: "term"] else {
+			throw Abort(.badRequest)
+		}
+		return Acronym.query(on: req.db)
+			.filter(\.$short == searchTerm)
+			.all()
+	}
 }
