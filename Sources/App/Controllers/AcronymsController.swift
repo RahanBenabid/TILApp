3ab7001c595f801a0625c003d21018356227d18e
RahanBenabid <rahannadime@gmail.com>
2024-07-31 19:24:28 +0100

    More controller
    MORE!!!!

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -3,11 +3,75 @@ import Fluent
 
 struct AcronymsController: RouteCollection {
 	func boot(routes: RoutesBuilder) throws {
-		func getAllHandler(_ req: Request) -> EventLoopFuture<[Acronym]> {
+		
+		// to generalise the path, since all of them use /api/acronyms
+		let acronymsRoutes = routes.grouped("api", "acronyms")
+		
+		@Sendable func getAllHandler(_ req: Request) -> EventLoopFuture<[Acronym]> {
 			Acronym.query(on: req.db).all()
 		}
+		
+		@Sendable func createHandler(_ req: Request) throws -> EventLoopFuture<Acronym> {
+			let acronym = try req.content.decode(Acronym.self)
+			return acronym.save(on: req.db).map { acronym }
+		}
+		
+		@Sendable func getHandler(_ req: Request) -> EventLoopFuture<Acronym> {
+			Acronym.find(req.parameters.get("acronymID"), on: req.db)
+				.unwrap(or: Abort(.notFound))
+		}
+		
+		@Sendable func updateHandler(_ req: Request) throws -> EventLoopFuture<Acronym> {
+			let updatedAcronym = try req.content.decode(Acronym.self)
+			return Acronym.find(req.parameters.get("acronymID"), on: req.db)
+				.unwrap(or: Abort(.notFound))
+				.flatMap { acronym in
+					acronym.short = updatedAcronym.short
+					acronym.long = updatedAcronym.long
+					return acronym.save(on: req.db).map {
+						acronym
+					}
+				}
+		}
+		
+		@Sendable func deleteHandler(_ req: Request) -> EventLoopFuture<HTTPStatus> {
+			Acronym.find(req.parameters.get("acronymID"), on: req.db)
+				.unwrap(or: Abort(.notFound))
+				.flatMap { acronym in
+					acronym.delete(on: req.db)
+						.transform(to: .noContent)
+				}
+		}
+		
+		@Sendable func searchHandler(_ req: Request) throws -> EventLoopFuture<[Acronym]> {
+			guard let searchTerm = req.query[String.self, at: "term"] else {
+				throw Abort(.badRequest)
+			}
+			return Acronym.query(on: req.db).group(.or) { or in
+				or.filter(\.$short == searchTerm)
+				or.filter(\.$long == searchTerm)
+			}.all()
+		}
+		
+		@Sendable func getFirstHandler(_ req: Request) -> EventLoopFuture<Acronym> {
+			return Acronym.query(on: req.db).first()
+				.unwrap(or: Abort(.notFound))
+		}
+		
+		@Sendable func sortedHandler(_ req: Request) -> EventLoopFuture<[Acronym]> {
+			return Acronym.query(on: req.db).sort(\.$short, .ascending).all()
+		}
+		
 				
-		// registering the routes
-		routes.get("api", "acronyms", use: getAllHandler)
+		// registering the routes handlers
+		acronymsRoutes.get(use: getAllHandler)
+		acronymsRoutes.post(use: createHandler)
+		acronymsRoutes.get(":acronymID", use: getHandler)
+		acronymsRoutes.put(":acronymID", use: updateHandler)
+		acronymsRoutes.delete(":acronymID", use: deleteHandler)
+		acronymsRoutes.get("search", use: searchHandler)
+		acronymsRoutes.get("first", use: getFirstHandler)
+		acronymsRoutes.get("sorted", use: sortedHandler)
+		
 	}
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,105 +2,18 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-	app.get { req async in
-		"It works!"
-	}
 	
 	app.get("hello") { req async -> String in
 		"Hello, world!"
 	}
 	
-	// the route api/acronyms accepts a POST req and returns an EvenLoopFuture<Acronym> which means it returns the acronym once its saved
-	app.post("api", "acronyms") { req -> EventLoopFuture<Acronym> in
-		// using Codable, we can decode the request JSON data into an Acronym
-		let acronym = try req.content.decode(Acronym.self)
-		// save the model
-		return acronym.save(on: req.db).map {
-			// returns the acronym when the save is complete and here we use .map
-			acronym
-		}
-	}
-	
-	// gets the value in the request path
-	app.get("api", "acronyms", ":acronymID") {
-		req -> EventLoopFuture<Acronym> in
-		// find() takes a UUID as a param
-		Acronym.find(req.parameters.get("acronymID"), on: req.db)
-		// since find() returns EventLoopFuture<Acronym?>, we handle this difference by adding unwrap()
-			.unwrap(or: Abort(.notFound))
-	}
-	
-	// update using the UUID
-	app.put("api", "acronyms", ":acronymID") {
-		req -> EventLoopFuture<Acronym> in
-		// decode the request body
-		let updatedAcronym = try req.content.decode(Acronym.self)
-		// find the acronym with the specified UUID
-		return Acronym.find(
-			req.parameters.get("acronymID"),
-			on: req.db)
-		// returns an error if not found
-		.unwrap(or: Abort(.notFound))
-		// waits for the future to complete, flatMap  is used because the operation inside involves another asynchronous operation, saving the updated acronym to the database.
-		.flatMap { acronym in
-			acronym.short = updatedAcronym.short
-			acronym.long = updatedAcronym.long
-			return acronym.save(on: req.db).map {
-				acronym
-			}
-		}
-	}
-	
-	// delete operation that returns an HTTPStatus
-	app.delete("api", "acronyms", ":acronymID") {
-		req -> EventLoopFuture<HTTPStatus> in
-		// exract the ID from the request
-		Acronym.find(req.parameters.get("acronymID"), on: req.db)
-			.unwrap(or: Abort(.notFound))
-		
-		// wait for the acronym to return from the database
-			.flatMap { acronym in
-				// deletes it
-				acronym.delete(on: req.db)
-				// trandforms the result into a 204 code: no result
-					.transform(to: .noContent)
-			}
-	}
-	
-	app.get("api", "acronyms", "search") {
-		req -> EventLoopFuture<[Acronym]> in
-		// retrieve the search term from the URL query string
-		guard let searchTerm = req.query[String.self, at: "term"] else {
-			throw Abort(.badRequest)
-		}
-		// find all the acronyms whos short prop matches
-		return Acronym.query(on: req.db).group(.or) { or in
-			or.filter(\.$short == searchTerm)
-			or.filter(\.$long == searchTerm)
-		}.all()
-	}
-	
-	// get only the first result
-	app.get("api", "acronyms", "first") {
-		req -> EventLoopFuture<Acronym> in
-		Acronym.query(on: req.db)
-			.first()
-			.unwrap(or: Abort(.notFound))
-	}
-	
-	// sorting the results
-	app.get("api", "acronyms", "sorted") {
-		req -> EventLoopFuture<[Acronym]> in
-		Acronym.query(on: req.db)
-			.sort(\.$short, .ascending)
-			.all()
-	}
-	
-	
 	/// better orgnanise routes
 	
 	// create a new AcronymController
 	let acronymsController = AcronymsController()
+	
 	// to ensure the route gets regitered
 	try app.register(collection: acronymsController)
+	
+	
 }
