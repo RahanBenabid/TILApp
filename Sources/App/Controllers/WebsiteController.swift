4f1d57c78cdc1f287dca93974261eecd4b5be642
RahanBenabid <rahannadime@gmail.com>
2024-08-13 19:36:39 +0100

    Delete, update acronyms/create, see categories

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -7,6 +7,13 @@ struct WebsiteController: RouteCollection {
 		routes.get("acronyms", ":acronymID", use: acronymHandler)
 		routes.get("users", ":userID", use: userHandler)
 		routes.get("users", use: allUserHandler)
+		routes.get("categories", use: allCategoriesHandler)
+		routes.get("categories", ":categoryID", use: categoryHandler)
+		routes.get("acronyms", "create", use: createAcronymHandler)
+		routes.post("acronyms", "create", use: createAcronymPostHandler)
+		routes.get("acronyms", ":acronymID", "edit", use: editAcronymHandler)
+		routes.post("acronyms", ":acronymID", "edit", use: editAcronymPostHandler)
+		routes.post("acronyms", ":acronymID", "delete", use: deleteAcronymHandler)
 	}
 	
 	@Sendable func indexHandler(_ req: Request) -> EventLoopFuture<View> {
@@ -36,7 +43,7 @@ struct WebsiteController: RouteCollection {
 			.unwrap(or: Abort(.notFound))
 			.flatMap { user in
 				user.$acronyms.get(on: req.db).flatMap { acronyms in
-					let context = userContext(title: "user.name",
+					let context = UserContext(title: "user.name",
 																		user: user,
 																		acronyms: acronyms)
 					return req.view.render("user", context)
@@ -48,9 +55,96 @@ struct WebsiteController: RouteCollection {
 		User.query(on: req.db)
 			.all()
 			.flatMap { users in
-			let context = allUsersContext(title: "All Users", users: users)
+			let context = AllUsersContext(title: "All Users", users: users)
 			return req.view.render("allUsers", context)
 		}
+	}
+	
+	@Sendable func allCategoriesHandler(_ req: Request) -> EventLoopFuture<View> {
+		Category.query(on: req.db).all().flatMap { categories in
+			let context = AllCategoriesContext(categories: categories)
+			return req.view.render("allCategories", context)
+		}
+	}
+	
+	@Sendable func categoryHandler(_ req: Request) -> EventLoopFuture<View> {
+		Category.find(req.parameters.get("categoryID"), on: req.db)
+			.unwrap(or: Abort(.notFound))
+			.flatMap { category in
+				category.$acronyms.get(on: req.db).flatMap { acronyms in
+					let context = CategoryContext(title: category.name,
+																				category: category,
+																				acronyms: acronyms)
+					return req.view.render("category", context)
+				}
+			}
+	}
+	
+	@Sendable func createAcronymHandler(_ req: Request) -> EventLoopFuture<View> {
+		User.query(on: req.db).all().flatMap { users in
+			let context = CreateAcronymContext(users: users)
+			return req.view.render("createAcronym", context)
+		}
+	}
+	
+	@Sendable func createAcronymPostHandler(_ req: Request) throws -> EventLoopFuture<Response> {
+		let data = try req.content.decode(CreateAcronymData.self)
+		let acronym = Acronym(
+			short: data.short,
+			long: data.long,
+			userID: data.userID)
+		
+		return acronym.save(on: req.db).flatMapThrowing {
+			// ensures the ID is set or else throws a server error
+			guard let id = acronym.id else {
+				throw Abort(.internalServerError)
+			}
+			return req.redirect(to: "/acronyms/\(id)")
+		}
+	}
+	
+	@Sendable func editAcronymHandler(_ req: Request) -> EventLoopFuture<View> {
+		let acronymFuture = Acronym
+			.find(req.parameters.get("acronymID"), on: req.db)
+			.unwrap(or: Abort(.notFound))
+		
+		let userQuery = User.query(on: req.db).all()
+		
+		return acronymFuture.and(userQuery)
+			.flatMap { acronym, users in
+				let context = EditAcronymContext(
+					acronym: acronym,
+					users: users)
+				return req.view.render("createAcronym", context)
+			}
+	}
+	
+	@Sendable func editAcronymPostHandler(_ req: Request) throws -> EventLoopFuture<Response> {
+		let updateData = try req.content.decode(CreateAcronymData.self)
+		
+		return Acronym
+			.find(req.parameters.get("acronymID"), on: req.db)
+			.unwrap(or: Abort(.notFound)).flatMap { acronym in
+				acronym.short = updateData.short
+				acronym.long = updateData.long
+				acronym.$user.id = updateData.userID
+				guard let id = acronym.id else {
+					let error = Abort(.internalServerError)
+					return req.eventLoop.future(error: error)
+				}
+				let redirect = req.redirect(to: "/acronyms/\(id)")
+				return acronym.save(on: req.db).transform(to: redirect)
+			}
+	}
+	
+	@Sendable func deleteAcronymHandler(_ req: Request) -> EventLoopFuture<Response> {
+		Acronym
+			.find(req.parameters.get("acronymID"), on: req.db)
+			.unwrap(or: Abort(.notFound))
+			.flatMap { acronym in
+				acronym.delete(on: req.db)
+					.transform(to: req.redirect(to: "/"))
+			}
 	}
 }
 
@@ -65,13 +159,36 @@ struct AcronymContext: Encodable {
 	let user: User
 }
 
-struct userContext: Encodable {
+struct UserContext: Encodable {
 	let title: String
 	let user: User
 	let acronyms: [Acronym]
 }
 
-struct allUsersContext: Encodable {
+struct AllUsersContext: Encodable {
 	let title: String
 	let users: [User]
+}
+
+struct AllCategoriesContext: Encodable {
+	let title = "All Categories"
+	let categories: [Category]
+}
+
+struct CategoryContext: Encodable {
+	let title: String
+	let category: Category
+	let acronyms: [Acronym]
+}
+
+struct CreateAcronymContext: Encodable {
+	let title = "Create an acronym" // static because the title doesn't change
+	let users: [User]
+}
+
+struct EditAcronymContext: Encodable {
+	let title = "Edit Acronym"
+	let acronym: Acronym
+	let users: [User]
+	let editing = true
 }
